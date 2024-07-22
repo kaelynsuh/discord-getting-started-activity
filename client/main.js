177ceb7e43df8a421eb0979c8088e7304027e3e6
Kaelyn Suh <kaelyn.suh@gmail.com>
2024-07-22 10:58:40 -0700

    LITERALLY word to word in docs

--- client/main.js
+++ client/main.js
@@ -1,7 +1,89 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+import rocketLogo from "/rocket.png";
+
+// Will eventually store the authenticated user's access_token
+let auth;
+
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  // console.log("Discord SDK is authenticated");
+  console.log(
+    "%c Discord SDK is authenticated           ",
+    "background: pink; color: black"
+  );
+
+  // We can now make API calls within the scopes we requested in setupDiscordSDK()
+  // Note: the access_token returned is a sensitive secret and should be treated as such
+  appendVoiceChannelName();
+});
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  // console.log("Discord SDK is ready");
+  console.log(
+    "%c Discord SDK is ready           ",
+    "background: pink; color: black"
+  );
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: ["identify", "guilds"],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
+
+async function appendVoiceChannelName() {
+  const app = document.querySelector("#app");
+
+  let activityChannelName = "Unknown";
+
+  // Requesting the channel in GDMs (when the guild ID is null) requires
+  // the dm_channels.read scope which requires Discord approval.
+  if (discordSdk.channelId != null && discordSdk.guildId != null) {
+    // Over RPC collect info about the channel
+    const channel = await discordSdk.commands.getChannel({
+      channel_id: discordSdk.channelId,
+    });
+    if (channel.name != null) {
+      activityChannelName = channel.name;
+    }
+  }
+
+  // Update the UI with the name of the current voice channel
+  const textTagString = `Activity Channel: "${activityChannelName}"`;
+  const textTag = document.createElement("p");
+  textTag.textContent = textTagString;
+  app.appendChild(textTag);
+}
+
+document.querySelector("#app").innerHTML = `
   <div>
     <img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
